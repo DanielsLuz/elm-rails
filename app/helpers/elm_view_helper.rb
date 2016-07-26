87a83c79c81846c670444f306fb63f869ee89ffa
DanielsLuz <danielluz1995@gmail.com>
2016-07-26 18:19:01 -0300

    Added wrapper for embed elm objects.

--- app/helpers/elm_view_helper.rb
+++ app/helpers/elm_view_helper.rb
@@ -1,22 +1,22 @@
 module ElmViewHelper
 
-  def elm_embed(module_name, elm_object_name)
+  def elm_embed(module_name, elm_object_id)
     content_tag(:div) do
-      component_div + script_tag(module_name, elm_object_name)
+      component_div(elm_object_id) + script_tag(module_name, elm_object_id)
     end
   end
 
   private
 
-  def component_div
-    content_tag(:div) {}
+  def component_div(elm_object_id)
+    content_tag(:div, "", id: elm_object_id)
   end
 
-  def script_tag(module_name, elm_object_name)
+  def script_tag(module_name, elm_object_id)
     content_tag(:script, type: "text/javascript") do
       render(file: "elm/mount_script.js.erb", 
              locals: { module_name: module_name,
-                       elm_object_name: elm_object_name })
+                       elm_object_id: elm_object_id })
     end
   end
 
